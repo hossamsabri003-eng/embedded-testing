fe5107adc312ef40ca721b98fa478bf715b7dfe7
Hossam Sbari <hossamsabri003@gmail.com>
2026-01-28 18:15:21 +0100

    Modification en code

--- test_of_speed.py
+++ test_of_speed.py
@@ -1,7 +1,7 @@
 from speed import check_speed
 
 def test_speed_ok():
-    assert check_speed(50, 80) == "OK"
+    assert check_speed(50, 80) == "OKK"
 
 def test_speed_overspeed():
     assert check_speed(120, 80) == "OVERSPEED"
